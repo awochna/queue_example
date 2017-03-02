38a500db74082531d1ae8bf9bceb56239df42611
Alex Wynter <awochna@email.arizona.edu>
2017-03-02 14:39:53 -0700

    Initial commit of generated stuff.

---
+++ lib/queue_example.ex
@@ -0,0 +1,18 @@
+defmodule QueueExample do
+  @moduledoc """
+  Documentation for QueueExample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> QueueExample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/queue_example_test.exs
@@ -0,0 +1,8 @@
+defmodule QueueExampleTest do
+  use ExUnit.Case
+  doctest QueueExample
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
